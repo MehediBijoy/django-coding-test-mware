6564144b553a8f2caa3178bee1611e74be89291f
Mehedi Hasan <mehedibubt34@gmail.com>
2023-12-26 18:42:42 +0600

    added product filters

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.db.models.query import QuerySet
 
 from product.models import Variant, Product, ProductVariant
 
@@ -24,6 +25,43 @@ class ProductListView(generic.ListView):
         context = super().get_context_data(**kwargs)
         variants = dict()
         for instance in Variant.objects.filter(active=True):
-            variants[instance.title] = ProductVariant.objects.filter(variant=instance).values('variant_title').distinct()
+            variants[instance.title] = ProductVariant.objects.filter(
+                variant=instance).values('variant_title').distinct()
         context['variants'] = variants
+        context['form_values'] = self.request.GET
         return context
+
+    def get_queryset(self) -> QuerySet[Product]:
+        queryset = super().get_queryset()
+
+        product_title = self.request.GET.get('title')
+        product_variant = self.request.GET.get('variant')
+        product_price_from = self.request.GET.get('price_from')
+        product_price_to = self.request.GET.get('price_to')
+        product_date = self.request.GET.get('date')
+
+        if product_title:
+            queryset = queryset.filter(title__icontains=product_title)
+
+        if product_variant:
+            queryset = queryset.filter(
+                productvariant__variant_title=product_variant
+            )
+
+        # Here is the ambiguity from django
+        # by default django handle distinct but here not work
+        # That's why I use distinct method here.
+        if product_price_from:
+            queryset = queryset.filter(
+                product_variant_prices__price__gte=product_price_from
+            ).distinct()
+
+        if product_price_to:
+            queryset = queryset.filter(
+                product_variant_prices__price__lte=product_price_to
+            ).distinct()
+
+        if product_date:
+            queryset = queryset.filter(created_at__gte=product_date)
+
+        return queryset
